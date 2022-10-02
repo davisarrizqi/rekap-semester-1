aeeadfc5353f5218adfec83d19d64f42ac12074a
Davis Arrizqi Putra Mandiri <85699695+davisapplication@users.noreply.github.com>
2022-10-02 15:50:42 +0700

    Tugas Hitung Luas (T3.5) - Standar Penugasan
    Ini adalah script yang digunakan untuk menyelesaikan tugas Algoritma dan Pemrograman Praktik yang ada pada halaman 28 pada modul. Lebih tepatnya, ini adalah Tugas 3.5

---
+++ hitung_luas.py
@@ -0,0 +1,21 @@
+'''
+Tugas Algoritma Pemrograman Praktik
+Nama : Davis Arrizqi Putra Mandiri
+NPM  : 5220411NUM
+'''
+
+                    # Algoritma HITUNG_LUAS
+# Deklarasi
+L = 10; P = 20; D = 14
+
+# Luas Persegi Panjang = P x L
+L_persegi = P * L
+
+# Luas Setengah Lingkaran = 0,5 x 3,14 x D/2 x D/2
+L_ling = 0.5 * 3.14 * D/2 * D/2
+
+# Luas Bangun = L_persegi + L_ling
+L_bangun = L_persegi + L_ling
+
+# Tampilkan L_bangun
+print(L_bangun)
