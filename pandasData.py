1d05348520f936da29536192cc3eb457da66ed28
Davis Arrizqi Putra Mandiri <85699695+davisarrizqi@users.noreply.github.com>
2022-11-30 10:03:47 +0700

    Update pandasData.py

--- pandasData.py
+++ pandasData.py
@@ -25,6 +25,41 @@ def latihanPertama():
 dataPenilaian = pandas.read_excel("Important Data/rekap_penilaian.xlsx")
 # print(dataPenilaian) --> debug our result
 
+
+
+#                              PERSOALAN
+
+
+# 1. Tambahkan kolom full name hasil gabungan dari first name dan last name
+
+# 2. Berapa nilai rata rata UAS dari data tersebut?
+
+# 3. Tambahkan kolom nilai akhir dengan ketentuan:
+    # Tugas 1 20%, Tugas 2 20%, UTS 25%, UAS 35%
+
+# 4. Tambahkan kolom Nilai Huruf dengan ketentuan nilai akhir:
+    # Lebih dari sama dengan 81, A
+    # Lebih dari sama dengan 61, B
+    # Lebih dari sama dengan 41, C
+    # Lebih dari sama dengan 21, D
+    # Lainnya E
+
+# 5. Tunjukkan nilai akhir dari mahasiswa ke 11 sampai dengan 20
+
+# 6. Tunjukkan nilai akhir dari mahasiswa yang memiliki First Name "Ryan"
+
+# 7. Hitung berapa jumlah mahasiswa Male dan Female
+
+# 8. Tampilkan hanya mahasiswa yang memiliki nilai huruf = A
+
+
+
+
+#                           PENYELESAIAN
+
+
 # 1. tambahkan kolom fullname hasil gabungan dari firstname dan last name
 dataPenilaian["Full Name"] = dataPenilaian["First Name"] + " " + dataPenilaian["Last Name"]
 print(dataPenilaian)
+
+# 2. Menghitung Nilai Rata Rata UAS
