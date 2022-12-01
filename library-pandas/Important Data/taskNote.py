2d0b04c9cac2e109aefc8ea515d47143ef7376a1
Davis Arrizqi Putra Mandiri <85699695+davisarrizqi@users.noreply.github.com>
2022-12-01 11:24:25 +0700

    Task Note - Pandas Module [1]

---
+++ library-pandas/Important Data/taskNote.py
@@ -0,0 +1,26 @@
+print("""
+                  P  E  R  S  O  A  L  A  N
+
+1. Tambahkan kolom full name hasil gabungan dari first name dan last name
+
+2. Berapa nilai rata rata UAS dari data tersebut?
+
+3. Tambahkan kolom nilai akhir dengan ketentuan:
+    Tugas 1 20%, Tugas 2 20%, UTS 25%, UAS 35%
+
+4. Tambahkan kolom Nilai Huruf dengan ketentuan nilai akhir:
+    Lebih dari sama dengan 81, A
+    Lebih dari sama dengan 61, B
+    Lebih dari sama dengan 41, C
+    Lebih dari sama dengan 21, D
+    Lainnya E
+
+5. Tunjukkan nilai akhir dari mahasiswa ke 11 sampai dengan 20
+
+6. Tunjukkan nilai akhir dari mahasiswa yang memiliki First Name "Ryan"
+
+7. Hitung berapa jumlah mahasiswa Male dan Female
+
+8. Tampilkan hanya mahasiswa yang memiliki nilai huruf = A
+
+                                                                    """)
