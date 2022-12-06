d2484470d458425e91e9cc294d95cd06050973f9
Davis Arrizqi Putra Mandiri <85699695+davisarrizqi@users.noreply.github.com>
2022-12-06 13:37:03 +0700

    Update firstPractice.py

--- library-matplotlib/firstPractice.py
+++ library-matplotlib/firstPractice.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy
 import random
 
+
+# algoritma saya sendiri
 def myWay():
     # part 1, declare it first
     semester = [1, 2, 3, 4, 5, 6]
@@ -24,6 +26,7 @@ def myWay():
     plt.bar(semester, ip, color=listWarnaDigunakan)
 
 
+# algoritma pak zaka
 def lecturerWay():
     semester = [1, 2, 3, 4, 5, 6]
     ip = [3.98, 3.56, 3.20, 3.01, 2.97, 3.75]
@@ -36,6 +39,8 @@ def lecturerWay():
     
     plt.bar(semester, ip, color=warna)
 
+
+# eksperimen saya
 def nextTrial():
     semester = [1, 2, 3, 4, 5, 6]
     ip = [3.98, 3.56, 3.20, 3.01, 2.97, 3.75]
@@ -48,6 +53,8 @@ def nextTrial():
     myGraph.set_xlabel("Garis X")
     plt.bar(semester, ip, color=warna)
 
+
+# algoritma pak zaka, menampilkan pie
 def pieWay():
     prodi = ["Sistem Informasi", "Informatika", "Psikologi"]
     jumlahMahasiswa = [145, 438, 186]
@@ -55,6 +62,7 @@ def pieWay():
     plt.pie(jumlahMahasiswa, labels=prodi, autopct="%i Mahasiswa")
 
 
+# algoritma saya, menggabungkan bar dan pie
 def joinTheGame():
     # bar declaration
     semester = [1, 2, 3, 4, 5, 6]
@@ -69,13 +77,8 @@ def joinTheGame():
     plt.subplot(1, 2, 2)
     plt.pie(jumlahMahasiswa, labels=prodi, autopct="%i Mahasiswa")
 
-def secondaryWay():
-    plt.subplot(1, 2, 1)
-    myWay()
 
-    plt.subplot(1, 2, 2)
-    pieWay()
-
+# algoritma pak zaka, menggabungkan bar dan pie
 def lecturerJoinWay():
     # bar declaration
     semester = [1, 2, 3, 4, 5, 6]
@@ -89,9 +92,44 @@ def lecturerJoinWay():
     # start process
     fig, ax = plt.subplots(1, 2)
 
-    # bar process
-    ax[0].set_title
-    ax[0].set_xlabel
-    ax[0].set_ylabel
+    # bar label
+    ax[0].set_title = "Grafik Batang Indeks Prestasi"
+    ax[0].set_xlabel = "Semester ke-"
+    ax[0].set_ylabel = "Nilai Indeks Prestasi"
 
-    ax[0].plot()
+    # pie label
+    ax[1].set_title = "Grafik Pie Jumlah Mahasiswa"
+
+    # set it up
+    ax[0].bar(semester, ip)
+    ax[1].pie(jumlahMahasiswa, labels=prodi, autopct="%i Mahasiswa")
+
+
+# bar declaration
+semester = [1, 2, 3, 4, 5, 6]
+ip = [3.98, 3.56, 3.20, 3.01, 2.97, 3.75]
+warna = ["#FF0000", "cyan", "blue", "magenta", "yellow", "#00FF00"]
+
+# plot declaration
+month = ["Jan", "Feb", "Mar", "Apr", 'May', "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
+sellPoint = [10, 30, 60, 75, 40, 30, 10, 20, 100, 120, 90, 70]
+
+# pie declaration
+prodi = ["Sistem Informasi", "Informatika", "Psikologi"]
+jumlahMahasiswa = [145, 438, 186]
+
+# declare our figure and axes
+fig, ax = plt.subplots(3, 3)
+
+# show the result
+ax[0, 0].bar(semester, ip)
+ax[1, 1].plot(month, sellPoint)
+ax[2, 2].pie(jumlahMahasiswa, labels=prodi, autopct="%i Mahasiswa")
+
+# disappear the blank one
+ax[0, 1].pie([0]); ax[0, 2].pie([0])
+ax[1, 0].pie([0]); ax[1, 2].pie([0])
+ax[2, 0].pie([0]); ax[2, 1].pie([0])
+
+# show the result
+plt.show()
